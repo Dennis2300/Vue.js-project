6ae573a5d798825c68885c2740ea3d20988e8968
Densanity <denniszixiang@gmail.com>
2025-02-01 17:36:21 +0100

    removed unnesecarry files

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <Background />
+  <InteractiveLogo />
+
+  <div class="content">
+    <h1>Welcome to the About Page</h1>
+    <router-link to="/">Go to Home</router-link>
+  </div>
+</template>
+
+<script setup></script>
+
---
+++ src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <Background />
+  <InteractiveLogo />
+
+  <div class="content">
+    <h1>Welcome to the Home Page</h1>
+    <router-link to="/about">Go to About</router-link>
+  </div>
+</template>
+
+<script setup></script>
+
