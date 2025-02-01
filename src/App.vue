43171e84c79feafd2d38e3bfd9039f70f6cea006
Densanity <denniszixiang@gmail.com>
2025-02-01 20:10:43 +0100

    added the navbar with routes and title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <router-view/>
+  <div class="navbar-container">
+    <div class="site-title">
+      <router-link to="/">Teyvat Bulletin</router-link>
+    </div>
+    <nav class="navbar">
+        <router-link to="/" class="navbar-text">Home</router-link> |
+        <router-link to="/about" class="navbar-text">About</router-link>
+    </nav>
+  </div>
+  <div class="content">
+    <router-view />
+  </div>
 </template>
 
 <script setup>
+import "./css/Navbar.css";
 </script>
