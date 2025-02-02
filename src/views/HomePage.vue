189b4b5e5c000c688cfe07f62aeff3e28af869ad
Densanity <denniszixiang@gmail.com>
2025-02-02 19:05:04 +0100

    Added header and intro to the homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,32 @@
 <template>
-  <div class="content">
-    <h1>Welcome to the Home Page</h1>
+  <div class="home-page-container">
+    <div class="home-page-header">
+      <h1>Welcome to Teyvat Bulletin</h1>
+    </div>
+    <div>
+      <div class="home-page-paragraph">
+        <p>
+          Hey there, Traveler! Teyvat Bulletin is a simple, fan-made space where
+          I share character builds, team compositions, and tips for Genshin
+          Impact. This isn't a pro-level guide—just a fun project where I
+          explore different playstyles and showcase builds that have worked for
+          me.
+        </p>
+      </div>
+      <div class="home-page-paragraph">
+          <ul class="custom-list">
+            <li>Character Builds – Recommended weapons, artifacts, and talent priorities.</li>
+            <li>Team Ideas – Synergies that can help in different parts of the game.</li>
+            <li>A Learning Journey – Just like you, I’m always experimenting and improving!</li>
+          </ul>
+      </div>
+      <div class="home-page-paragraph">
+        I hope you find something useful here, and if you have any cool builds or ideas, I'd love to hear them! Let's explore Teyvat together! 🌿✨
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import "./../css/HomePage.css";
 </script>
-
