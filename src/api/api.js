27ba626c4c1eb666139e2a47c52eba027e1357c3
Densanity <denniszixiang@gmail.com>
2025-02-03 19:41:09 +0100

    added baseURL for future API calls

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const API_URL = "https://gsi.fly.dev";
 
 const apiClient = axios.create({
+  baseURL: API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
@@ -12,7 +13,7 @@ const apiClient = axios.create({
 // API call to get all characters
 export const getCharacters = async () => {
   try {
-    const response = await apiClient.get("https://gsi.fly.dev/characters");
+    const response = await apiClient.get("/characters");
     return response.data;
   } catch (error) {
     console.error("Error getting characters", error);
