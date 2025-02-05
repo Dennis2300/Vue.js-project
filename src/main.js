42af735827ebb720f394a08c92ad6fa2a52b0860
Densanity <denniszixiang@gmail.com>
2025-02-05 19:43:55 +0100

    renamed global.css with uppercase C

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./css/global.css"
+import "./css/Global.css"
 
 createApp(App).use(router).mount("#app");
