ff38ed99119b766dc9a2c4f0e01a17c8f64e4a24
Densanity <denniszixiang@gmail.com>
2025-02-08 21:20:11 +0100

    added the router

--- src/router.js
+++ src/router.js
@@ -3,12 +3,15 @@ import HomePage from "./views/HomePage.vue";
 import CharactersPage from "./views/CharactersPage.vue";
 import TeamsPage from "./views/TeamsPage.vue";
 import AboutPage from "./views/AboutPage.vue";
+import TestDataPage from "./views/TestDataPage.vue";
 
 const routes = [
   { path: "/", component: HomePage },
   { path: "/about", component: AboutPage },
   { path: "/characters", component: CharactersPage },
   { path: "/teams", component: TeamsPage },
+  { path: "/test", component: TestDataPage },
+
 ];
 
 const router = createRouter({
