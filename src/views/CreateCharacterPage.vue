99b8ed510ab2f6f5bfd5b061a6869092a763ba73
Densanity <denniszixiang@gmail.com>
2025-02-10 17:59:25 +0100

    fetch all visions on create character page

---
+++ src/views/CreateCharacterPage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { supabase } from "../supabaseClient.js";
+
+const visions = ref([]);
+const error = ref(null);
+
+async function fetchVisions() {
+    try {
+        let { data, error: fetchError } = await supabase.from("vision").select("*");
+        if (fetchError) throw fetchError;
+        visions.value = data;
+        console.log(data);
+    } catch (err) {
+        error.value = err.message;
+    }
+};
+
+onMounted(() => {
+    fetchVisions();
+});
+</script>
+
+<template>
+  <div>
+    <h2>Create Character</h2>
+    <form ></form>
+  </div>
+</template>
