958cac233605bfd39d42497d4aad2faee27a3461
Densanity <denniszixiang@gmail.com>
2025-02-12 18:52:34 +0100

    Minor changes to bulletin board
    - fix some margin and padding
    - added pictures to the notes
    - seperated board and note css to another file

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,14 +4,23 @@
       <h1>Welcome to Teyvat Bulletin</h1>
     </div>
     <div class="board-container">
-        <div class="board">
-          <div class="note">Latest News</div>
-          <div class="note">Event Updates</div>
-          <div class="note">Patch Notes</div>
-          <div class="note">Community Posts</div>
-          <div class="note">Community Posts</div>
-          <div class="note">Community Posts</div>
+      <div class="board">
+        <div class="note">
+          <img
+            src="https://act-upload.hoyoverse.com/event-ugc-hoyowiki/2024/09/15/35428890/4c69147dbf57b2f43e4ae3de6d555b88_8673991642896185661.png?x-oss-process=image%2Fformat%2Cwebp"
+            alt=""
+          />
         </div>
+        <div class="note">Welcome To Teyvat Bulletin</div>
+        <div class="note">Patch Notes</div>
+        <div class="note">Community Posts</div>
+        <div class="note">
+          <img
+            src="https://act-upload.hoyoverse.com/event-ugc-hoyowiki/2024/05/20/35428890/fe8fbea8637da1774e3063fc53b2428a_2056914089388248431.png?x-oss-process=image%2Fformat%2Cwebp"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
 
     <!-- Footer Section -->
@@ -34,4 +43,5 @@
 
 <script setup>
 import "./../css/HomePage.css";
+import "./../css/BulletinBoard.css";
 </script>
