f9a5f187de3f6af11a7236af3166ff65e47cc6fc
Densanity <denniszixiang@gmail.com>
2025-02-12 18:14:21 +0100

    Made a bulletin board on home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,8 +3,15 @@
     <div class="home-page-header">
       <h1>Welcome to Teyvat Bulletin</h1>
     </div>
-    <div>
-      <div class="home-page-paragraph"></div>
+    <div class="board-container">
+        <div class="board">
+          <div class="note">Latest News</div>
+          <div class="note">Event Updates</div>
+          <div class="note">Patch Notes</div>
+          <div class="note">Community Posts</div>
+          <div class="note">Community Posts</div>
+          <div class="note">Community Posts</div>
+        </div>
     </div>
 
     <!-- Footer Section -->
@@ -13,9 +20,9 @@
         <h3>Disclaimer!</h3>
         <p>
           This website is a fan-made project and is not affiliated with,
-          endorsed by, or sponsored by HoYoverse or miHoYo. <br> Character,
-          images, names, and assets used on this site are the property of
-          HoYoverse.
+          endorsed by, or sponsored by HoYoverse or miHoYo. <br />
+          Character, images, names, and assets used on this site are the
+          property of HoYoverse.
         </p>
         <div class="home-page-footer-link">
           Find more info <a href="/about">here</a>
