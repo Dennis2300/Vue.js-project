eac0539fd534fa2eccc010c67542515567aa493f
Densanity <denniszixiang@gmail.com>
2025-02-13 16:53:13 +0100

    Update AboutPage.vue

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,13 +1,18 @@
 <template>
-  <Background />
-  <InteractiveLogo />
+  <div class="about-page">
+    <section ref="intro" class="section">
+      <h1>About this project</h1>
+      <p>This project is fan made</p>
+    </section>
 
-  <div class="content">
-    <h1>Welcome to the About Page</h1>
+    <section>
+      <h2>Techstack used!</h2>
+      <ul>
+        <li>Vue.JS</li>
+        <li>GSAP</li>
+      </ul>
+    </section>
   </div>
 </template>
 
-<script setup>
-import "./../css/AboutPage.css";
-</script>
-
+<script setup></script>
