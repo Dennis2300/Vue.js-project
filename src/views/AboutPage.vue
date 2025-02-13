cfa1fc75379c43619ec932aca243c2a00e4e78b2
Densanity <denniszixiang@gmail.com>
2025-02-13 18:54:26 +0100

    setup the initial about page
    need more styling and some gsap animations

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="about-page-container">
+    <!--About this project section-->
+    <section
+      ref="purposeSection"
+      class="section"
+      :style="{
+        backgroundImage:
+          'url(https://upload-static.hoyoverse.com/hoyolab-wiki/2023/06/03/77454259/73407650482515e4bcdffeb4192a54d1_8824472991429103822.png?x-oss-process=image%2Fformat%2Cwebp)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }"
+    >
+      <h1>About this project</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, culpa
+        reiciendis tempore corporis exercitationem delectus est voluptas eveniet
+        quod? Ratione soluta ex aliquam aspernatur sint fugit explicabo
+        voluptatibus quis facilis.
+      </p>
+    </section>
+
+    <!--Disclaimer section-->
+    <section ref="disclaimerSection" class="section">
+      <h1>Disclaimer!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        incidunt iste explicabo itaque dicta iure cumque, tenetur suscipit!
+        Numquam quae eligendi voluptate rem voluptatibus ullam, in totam
+        repudiandae doloremque expedita!
+      </p>
+    </section>
+
+    <!--Credits section-->
+    <section ref="creditsSection" class="section">
+      <h1>Credits</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        incidunt iste explicabo itaque dicta iure cumque, tenetur suscipit!
+        Numquam quae eligendi voluptate rem voluptatibus ullam, in totam
+        repudiandae doloremque expedita!
+      </p>
+    </section>
+
+    <!--Techstack Used section-->
+    <section ref="techstackSection" class="section">
+      <h1>Techstack Used</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis eum
+        quisquam vitae! Eos sapiente accusamus minima nulla aliquam, praesentium
+        saepe modi eius dolore, perferendis omnis error magnam amet laboriosam
+        quis?
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+import "./../css/Tailwind.css";
+import "./../css/AboutPage.css";
+</script>
