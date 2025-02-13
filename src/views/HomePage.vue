7125a560d1558216490ff894b4616de0e59d5d34
Densanity <denniszixiang@gmail.com>
2025-02-13 16:22:54 +0100

    added text to the second note

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,22 +12,29 @@
           />
         </div>
         <div class="note">
-          Hey there, Traveler! Teyvat Bulletin is a simple, fan-made space where
-          I share character builds, team compositions, and tips for Genshin
-          Impact. This isn't a pro level guide, just a fun project where I
-          explore different playstyles and showcase builds that have worked for
-          me.
+          <strong>Hey there, Traveler!</strong>
+          <p>
+            Teyvat Bulletin is a simple, fan-made space where I share character
+            builds, team compositions, and tips for Genshin Impact. <br />This
+            isn't a pro-level guide, just a fun project where I explore
+            different playstyles and showcase builds that have worked for me.
+          </p>
+        </div>
+
+        <div class="note">
+          <strong>What to expect?</strong>
+          <p>
+            Here you will find character build, character materials guide, and
+            team compositions
+          </p>
         </div>
         <div class="note">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eaque
-          totam facilis beatae labore incidunt blanditiis consequuntur, voluptas
-          magnam velit nulla illo quas sint ut amet omnis rem, autem voluptates!
-        </div>
-        <div class="note">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus qui
-          minus consectetur necessitatibus molestias odit nostrum facilis at
-          cupiditate vel, repellat distinctio similique maiores voluptatibus,
-          eos esse pariatur commodi. Iusto!
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus qui
+            minus consectetur necessitatibus molestias odit nostrum facilis at
+            cupiditate vel, repellat distinctio similique maiores voluptatibus,
+            eos esse pariatur commodi. Iusto!
+          </p>
         </div>
         <div class="note">
           <img
