7bde22a1fa5393a66b37b0f3aecbb6645359c45b
Densanity <denniszixiang@gmail.com>
2025-02-13 17:21:31 +0100

    Succesfully installed tailwind
    had issue with installing tailwind but now it should be up and working. And I can now use tailwind

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./css/Global.css"
+import "./css/global.css";
 
 createApp(App).use(router).mount("#app");
