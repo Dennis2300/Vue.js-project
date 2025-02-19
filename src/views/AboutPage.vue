ba16e268a6445a32a2c246510241c0c26df9795b
Densanity <denniszixiang@gmail.com>
2025-02-19 18:21:38 +0100

    animated title header

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about-page-container">
     <!--About this project section-->
-    <section ref="purposeSection" class="section">
-      <h1>About this project</h1>
+    <section class="section">
+      <h1 class="title">About this project</h1>
       <p>
         I started <strong class="text-highlight">Teyvat Bulletin</strong> as a
         way to learn and improve my programming skills, particularly in Vue.js,
@@ -12,7 +12,7 @@
         experimenting with web effects and interactive UI elements to make the
         site visually engaging.
       </p>
-      <p>
+      <p class="paragraph">
         At the same time, I’ve always enjoyed
         <strong class="text-highlight">Genshin Impact</strong>, and I wanted to
         create a space where players can easily find character materials, weapon
@@ -21,7 +21,7 @@
         user-friendly resource that helps both new and experienced players
         quickly see what they need to level up their favorite characters.
       </p>
-      <p>
+      <p class="paragraph">
         This project is a combination of my interest in programming, web design,
         and Genshin Impact, and it continues to evolve as I learn more about
         Vue, front-end development, and interactive web experiences.
@@ -29,9 +29,9 @@
     </section>
 
     <!--Disclaimer section-->
-    <section ref="disclaimerSection" class="section">
+    <section class="disclaimerSection">
       <h1>Disclaimer!</h1>
-      <p>
+      <p class="disclaimer-section-paragraph">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
         incidunt iste explicabo itaque dicta iure cumque, tenetur suscipit!
         Numquam quae eligendi voluptate rem voluptatibus ullam, in totam
@@ -40,7 +40,7 @@
     </section>
 
     <!--Credits section-->
-    <section ref="creditsSection" class="section">
+    <section class="creditsSection">
       <h1>Credits</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
@@ -51,7 +51,7 @@
     </section>
 
     <!--Techstack Used section-->
-    <section ref="techstackSection" class="section">
+    <section class="techstackSection">
       <h1>Techstack Used</h1>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis eum
@@ -67,4 +67,29 @@
 import "./../css/Tailwind.css";
 import "./../css/AboutPage.css";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { onMounted } from "vue";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function animateTitle() {
+  gsap.fromTo(
+    ".title",
+    {
+      opacity: 0,
+      y: -100,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+    }
+  );
+}
+
+
+onMounted(() => {
+  animateTitle();
+});
 </script>
