c111f4821e4550d13f906fd826d25b60fe89e8dc
Densanity <denniszixiang@gmail.com>
2025-02-19 19:43:20 +0100

    redone animation in about page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="about-page-container">
-    <!--About this project section-->
-    <section class="section">
+    <div class="about-page-intro">
+      <!--About this project section-->
       <h1 class="title">About this project</h1>
-      <p>
+      <p class="paragraph">
         I started <strong class="text-highlight">Teyvat Bulletin</strong> as a
         way to learn and improve my programming skills, particularly in Vue.js,
         since it's the framework
@@ -12,7 +12,7 @@
         experimenting with web effects and interactive UI elements to make the
         site visually engaging.
       </p>
-      <p class="paragraph">
+      <p>
         At the same time, I’ve always enjoyed
         <strong class="text-highlight">Genshin Impact</strong>, and I wanted to
         create a space where players can easily find character materials, weapon
@@ -21,45 +21,48 @@
         user-friendly resource that helps both new and experienced players
         quickly see what they need to level up their favorite characters.
       </p>
-      <p class="paragraph">
+      <p>
         This project is a combination of my interest in programming, web design,
         and Genshin Impact, and it continues to evolve as I learn more about
         Vue, front-end development, and interactive web experiences.
       </p>
-    </section>
+    </div>
 
-    <!--Disclaimer section-->
-    <section class="disclaimerSection">
-      <h1>Disclaimer!</h1>
-      <p class="disclaimer-section-paragraph">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        incidunt iste explicabo itaque dicta iure cumque, tenetur suscipit!
-        Numquam quae eligendi voluptate rem voluptatibus ullam, in totam
-        repudiandae doloremque expedita!
-      </p>
-    </section>
+    <!--Animation container-->
+    <div class="animation-container">
+      <!--Disclaimer section-->
+      <section>
+        <h1>Disclaimer!</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+          incidunt iste explicabo itaque dicta iure cumque, tenetur suscipit!
+          Numquam quae eligendi voluptate rem voluptatibus ullam, in totam
+          repudiandae doloremque expedita!
+        </p>
+      </section>
 
-    <!--Credits section-->
-    <section class="creditsSection">
-      <h1>Credits</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        incidunt iste explicabo itaque dicta iure cumque, tenetur suscipit!
-        Numquam quae eligendi voluptate rem voluptatibus ullam, in totam
-        repudiandae doloremque expedita!
-      </p>
-    </section>
+      <!--Credits section-->
+      <section>
+        <h1>Credits</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+          incidunt iste explicabo itaque dicta iure cumque, tenetur suscipit!
+          Numquam quae eligendi voluptate rem voluptatibus ullam, in totam
+          repudiandae doloremque expedita!
+        </p>
+      </section>
 
-    <!--Techstack Used section-->
-    <section class="techstackSection">
-      <h1>Techstack Used</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis eum
-        quisquam vitae! Eos sapiente accusamus minima nulla aliquam, praesentium
-        saepe modi eius dolore, perferendis omnis error magnam amet laboriosam
-        quis?
-      </p>
-    </section>
+      <!--Techstack Used section-->
+      <section>
+        <h1>Techstack Used</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+          eum quisquam vitae! Eos sapiente accusamus minima nulla aliquam,
+          praesentium saepe modi eius dolore, perferendis omnis error magnam
+          amet laboriosam quis?
+        </p>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -88,8 +91,24 @@ function animateTitle() {
   );
 }
 
+function animateParagraph() {
+  gsap.fromTo(
+    ".about-page-intro p",
+    {
+      opacity: 0,
+      y: 100,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.2,
+    }
+  );
+}
 
 onMounted(() => {
   animateTitle();
+  animateParagraph();
 });
 </script>
