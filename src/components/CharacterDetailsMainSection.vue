6b023ef8689323fe61ac64bd4faee1d9aa9088f0
Densanity <denniszixiang@gmail.com>
2025-03-19 19:31:04 +0100

    Update CharacterDetailsMainSection.vue

--- src/components/CharacterDetailsMainSection.vue
+++ src/components/CharacterDetailsMainSection.vue
@@ -1,38 +1,48 @@
 <template>
-      <section class="main-section grid">
-          <div class="grid-item-name">
-            {{ character.vision.name }}
-          </div>
-          <div class="grid-item-birthday">
-            {{ character.birthday }}
-          </div>
-          <div class="grid-item-constellation">
-            {{ character.constellation }}
-          </div>
-          <div>
-            {{ character.favourite_dish }}
-          </div>
-          <div>
-            {{ character.release_date }}
-          </div>
-          <div class="grid-item-affiliation">
-            <ul class="styled-list">
-              <li
-                v-for="(affiliation, index) in character.affiliation"
-                :key="index"
-              >
-                {{ affiliation }}
-              </li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <div v-for="(actor, language) in character.va" :key="language">
-                {{ language }} Voice Actor: <strong>{{ actor }}</strong>
-              </div>
-            </ul>
-          </div>
-        </section>
+  <section class="main-section grid">
+    <!-- Name -->
+    <div class="grid-item-name">
+      {{ character.vision.name }}
+    </div>
+    
+    <!-- Birthday -->
+    <div class="grid-item-birthday">
+      {{ character.birthday }}
+    </div>
+
+    <!-- Constellation -->
+    <div class="grid-item-constellation">
+      {{ character.constellation }}
+    </div>
+
+    <!-- Favourite Dish -->
+    <div class="grid-item-favourite-dish">
+      {{ character.favourite_dish }}
+    </div>
+
+    <!-- Release Date -->
+    <div class="grid-item-release-date">
+      {{ character.release_date }}
+    </div>
+
+    <!-- Affiliation -->
+    <div class="grid-item-affiliation">
+      <ul class="styled-list">
+        <li v-for="(affiliation, index) in character.affiliation" :key="index">
+          {{ affiliation }}
+        </li>
+      </ul>
+    </div>
+
+    <!-- Vision -->
+    <div>
+      <ul>
+        <div v-for="(actor, language) in character.va" :key="language">
+          {{ language }} Voice Actor: <strong>{{ actor }}</strong>
+        </div>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script setup>
