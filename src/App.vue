ee282e9f70e3c02fb3437e4ee5ec13e72b9b16ac
Densanity <denniszixiang@gmail.com>
2025-04-27 19:23:21 +0200

    redesigned navbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
-  <!-- Navbar Section -->
-  <div class="navbar-container fixed m-0 p-2">
-    <div class="site-title pl-5">
-      <router-link to="/"
-        ><p class="shining-text">Teyvat Bulletin</p></router-link
-      >
-    </div>
-    <nav class="navbar">
-      <router-link to="/" class="navbar-text">Home</router-link> |
-      <router-link to="/characters" class="navbar-text">Characters</router-link>
-      | <router-link to="/teams" class="navbar-text">Teams</router-link> |
-      <router-link to="/about" class="navbar-text">About</router-link>
-    </nav>
-  </div>
+  <!-- Navbar Component -->
+  <Navbar />
 
   <!-- Main Content Section -->
   <div class="content">
@@ -21,5 +9,5 @@
 </template>
 
 <script setup>
-import "./css/Navbar.css";
+import Navbar from "./components/Navbar.vue";
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar-container">
+    <nav class="web-navbar">
+      <img src="../../public/webIcon.webp" alt="Logo" class="favicon" />
+      <router-link to="/">Home</router-link>
+      <router-link to="/characters">Character</router-link>
+      <router-link to="/weapons">Weapons</router-link>
+      <router-link to="/artifacts">Artifacts</router-link>
+      <router-link to="/teams">Team</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+import "./../css/Navbar.css";
+</script>
