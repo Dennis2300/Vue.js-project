819fd1569a8a4804a27d0b0aabbc335e400e69b4
Densanity <denniszixiang@gmail.com>
2025-04-30 20:10:45 +0200

    created page
    created artifacts page and added route

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import AboutPage from "./views/AboutPage.vue";
 import TestDataPage from "./views/TestDataPage.vue";
 import CharacterDetailPage from "./views/CharacterDetailPage.vue";
 import WeaponsPage from "./views/WeaponsPage.vue";
+import ArtifactsPage from "./views/ArtifactsPage.vue";
 
 const routes = [
   { path: "/", component: HomePage },
@@ -14,6 +15,7 @@ const routes = [
   { path: "/teams", component: TeamsPage },
   { path: "/test", component: TestDataPage },
   { path: "/weapons", component: WeaponsPage },
+  { path: "/artifacts", component: ArtifactsPage },
   {
     path: "/characters/:id",
     name: "CharacterDetaiPage",
---
+++ src/views/ArtifactsPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="artifacts-container">
+      <h1>Welcome to the Artifacts Page</h1>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped>
+    .artifacts-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+    }
+  
+    h1 {
+      font-size: 2rem;
+      color: white;
+    }
+  </style>
