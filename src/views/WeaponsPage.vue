9e1479c429c9ecda98bcfa26f809d164d3d0248b
Densanity <denniszixiang@gmail.com>
2025-05-07 20:39:24 +0200

    styled the weapon table

--- src/views/WeaponsPage.vue
+++ src/views/WeaponsPage.vue
@@ -6,14 +6,60 @@
       <div class="filter-header">Placeholder for Weapons Filter</div>
     </div>
 
-    <div class="weapons-grid-container" v-if="!loading && !error">
-      <h1 class="weapons-page-header divider">Welcome to the Weapons Page</h1>
-      <div class="weapons-grid">
-        <div v-for="weapon in weapons" :key="weapon.id">
-          <h1>{{ weapon.name }}</h1>
-          <img :src="weapon.image_url" :alt="weapon.name" />
-        </div>
-      </div>
+    <div class="weapons-table-container" v-if="!loading && !error">
+      <h1 class="weapons-page-header divider">Weapon Archive</h1>
+      <table class="weapons-table">
+        <colgroup>
+          <!-- Icon column -->
+          <col style="width: 50px" />
+          <!-- Name column -->
+          <col style="width: 200px" />
+          <!-- Rarity column -->
+          <col style="width: 80px" />
+          <!-- Type column -->
+          <col style="width: 100px" />
+          <!-- Base Attack column -->
+          <col style="width: 100px" />
+          <!-- Bonus Effect column -->
+          <col style="width: 150px" />
+          <!-- Bonus Effect Value column -->
+          <col style="width: 150px" />
+          <!-- View More column -->
+          <col style="width: 25px" />
+        </colgroup>
+        <tr>
+          <th class="weapon-icon"></th>
+          <th>Name</th>
+          <th>Rarity</th>
+          <th>Type</th>
+          <th>Base Attack</th>
+          <th>Bonus Effect Type</th>
+          <th>Bonus Effect</th>
+        </tr>
+        <tr class="table-data" v-for="(weapons, index) in weapons" :key="index">
+          <td>
+            <img
+              class="weapon-icon"
+              :src="weapons.image_url"
+              :alt="weapons.image_url"
+            />
+          </td>
+          <td>{{ weapons.name }}</td>
+          <td>{{ weapons.rarity }}</td>
+          <td>{{ weapons.weapon_type_id.name }}</td>
+          <td>{{ weapons.base_attack }}</td>
+          <td>{{ weapons.bonus_effect_type_id.name }}</td>
+          <td>
+            {{
+              formatBonusEffectValue(
+                weapons.bonus_effect_value,
+                weapons.bonus_effect_type_id.name
+              )
+            }}
+          </td>
+          <td><button>More...</button></td>
+        </tr>
+      </table>
     </div>
 
     <div v-if="!loading && !error">
@@ -35,7 +81,9 @@ const weapons = ref([]);
 
 async function getAllWeapons() {
   try {
-    let { data, error: fetchError } = await supabase.from("weapon").select("*");
+    let { data, error: fetchError } = await supabase
+      .from("weapon")
+      .select("*, weapon_type_id(name), bonus_effect_type_id(name)");
 
     if (fetchError) throw fetchError;
 
@@ -45,6 +93,11 @@ async function getAllWeapons() {
   } finally {
     loading.value = false;
   }
+}
+
+function formatBonusEffectValue(value, type) {
+  // add more types here
+  return ["CRIT DMG", "CRIT Rate"].includes(type) ? `${value} %` : value;
 }
 
 onMounted(() => {
@@ -58,19 +111,31 @@ onMounted(() => {
   width: 100vw;
 }
 
-.weapons-grid-container {
-  min-height: 100vh;
-}
-
-.weapons-page-header {
-  font-size: 2rem;
-  color: white;
-}
-
 .filter-container {
   display: flex;
   justify-content: center;
   align-items: center;
   margin-bottom: 20px;
+  height: 7vh;
+}
+
+.weapons-table-container {
+  min-height: 100vh;
+}
+
+.weapons-table {
+  margin: 0 auto;
+  width: 1400px;
+}
+
+.weapon-icon {
+  width: 80px;
+  height: 80px;
+}
+
+.table-data {
+  text-align: center;
+  font-size: 1.5em;
+  font-family: var(--font-acme);
 }
 </style>
