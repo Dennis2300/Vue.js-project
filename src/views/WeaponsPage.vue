302062ec384fcf9e84d543c0896927c077badc73
Densanity <denniszixiang@gmail.com>
2025-05-11 17:39:45 +0200

    Update WeaponsPage.vue

--- src/views/WeaponsPage.vue
+++ src/views/WeaponsPage.vue
@@ -27,7 +27,7 @@
           <!-- View More column -->
           <col style="width: 25px" />
         </colgroup>
-        <tr>
+        <tr class="table-header">
           <th class="weapon-icon"></th>
           <th>Name</th>
           <th>Rarity</th>
@@ -35,17 +35,24 @@
           <th>Base Attack</th>
           <th>Bonus Effect</th>
           <th>Bonus Value</th>
+          <th></th>
         </tr>
         <tr class="table-data" v-for="(weapons, index) in weapons" :key="index">
           <td>
             <img
               class="weapon-icon"
+              :class="{
+                'rarity-5': weapons.rarity === 5,
+                'rarity-4': weapons.rarity === 4,
+              }"
               :src="weapons.image_url"
               :alt="weapons.image_url"
             />
           </td>
           <td>{{ weapons.name }}</td>
-          <td>{{ weapons.rarity }}</td>
+          <td>
+            <p class="rarity-text text-white" :data-stars="weapons.rarity"></p>
+          </td>
           <td>{{ weapons.weapon_type_id.name }}</td>
           <td>{{ weapons.base_attack }}</td>
           <td>{{ weapons.bonus_effect_type_id.name }}</td>
@@ -122,30 +129,56 @@ onMounted(() => {
 
 .weapons-table-container {
   margin-top: 25px;
-  height: 75vh;
+  height: 55vh;
   overflow-y: scroll;
   margin-bottom: 25px;
 }
 
 .weapons-table {
   margin: 0 auto;
-  width: 1500px;
+  width: 100vw;
   border-collapse: collapse;
-  border: 1px solid var(--tertiary);
 }
 
-.weapons-table tr {
-  border-bottom: 1px solid #ccc;
+.table-header {
+  background-color: #0b192c;
+  font-family: var(--font-acme);
+  font-size: 2em;
+  position: sticky;
+  top: 0%;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.75);
+}
+
+.weapons-table tr:nth-child(odd) {
+  background-color: var(--secondary);
 }
 
 .weapon-icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 80px;
   height: 80px;
+  border-radius: 20px;
+  margin: 10px;
 }
 
 .table-data {
   text-align: center;
   font-size: 1.5em;
-  font-family: var(--font-acme);
+  font-family: Arial, Helvetica, sans-serif;
+  letter-spacing: 1px;
+}
+
+.rarity-5 {
+  background: linear-gradient(145deg, #e7944a, #b56a2b);
+  box-shadow: 0px 0px 5px rgba(231, 148, 74, 0.8),
+    0px 0px 15px rgba(231, 148, 74, 0.5);
+}
+
+.rarity-4 {
+  background: linear-gradient(145deg, #9b72d5, #7149a3);
+  box-shadow: 0px 0px 5px rgba(155, 114, 213, 0.8),
+    0px 0px 15px rgba(155, 114, 213, 0.5);
 }
 </style>
