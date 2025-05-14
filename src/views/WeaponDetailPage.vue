37432919e7b98deec6db5e0597ee7739dbbcbc28
Densanity <denniszixiang@gmail.com>
2025-05-14 18:31:31 +0200

    preliminary weapon detail page

--- src/views/WeaponDetailPage.vue
+++ src/views/WeaponDetailPage.vue
@@ -2,9 +2,26 @@
   <div class="weapon-detail-container">
     <LoadingSpinner v-if="loading" />
 
-    <div v-else-if="weapon">
-      <h1>{{ weapon.name }}</h1>
+    <div v-else-if="weapon" class="weapon-detail">
       <router-link to="/weapons">Back to weapons</router-link>
+      <h1>{{ weapon.name }}</h1>
+      <img :src="weapon.image_url" :alt="weapon.name" />
+      <p>{{ weapon.rarity }}</p>
+      <p>{{ weapon.weapon_type_id.name }}</p>
+      <p>{{ weapon.base_attack }}</p>
+      <p>{{ weapon.bonus_effect_type_id.name }}</p>
+      <p>
+        {{
+          formatBonusEffectValue(
+            weapon.bonus_effect_value,
+            weapon.bonus_effect_type_id.name
+          )
+        }}
+      </p>
+      <p>{{ weapon.release_date }}</p>
+      <p>{{ weapon.lore_description }}</p>
+      <p>{{ weapon.attribute }}</p>
+      <p>{{ weapon.attribute_description }}</p>
     </div>
   </div>
 </template>
@@ -24,7 +41,7 @@ async function fetchWeaponDetails(weaponId) {
   try {
     const { data: weaponData, error: weaponsError } = await supabase
       .from("weapons")
-      .select("*")
+      .select("*, weapon_type_id(name), bonus_effect_type_id(name)")
       .eq("id", weaponId)
       .single();
     if (weaponsError) throw weaponsError;
@@ -34,6 +51,20 @@ async function fetchWeaponDetails(weaponId) {
     console.log("Error fetching weapon:", err);
     return null;
   }
+}
+
+function formatBonusEffectValue(value, type) {
+  // add more types here
+  return [
+    "CRIT DMG",
+    "CRIT Rate",
+    "DEF %",
+    "ATK %",
+    "HP %",
+    "Energy Recharge",
+  ].includes(type)
+    ? `${value}%`
+    : value;
 }
 
 onMounted(async () => {
@@ -47,5 +78,9 @@ onMounted(async () => {
 <style scoped>
 .weapon-detail-container {
   margin: 75px 0 0 0;
+}
+
+.weapon-detail {
+  background-color: aqua;
 }
 </style>
