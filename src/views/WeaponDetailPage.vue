a36aa8c8abba89238738a3348d279713518fd769
Densanity <denniszixiang@gmail.com>
2025-05-15 19:23:08 +0200

    Update WeaponDetailPage.vue

--- src/views/WeaponDetailPage.vue
+++ src/views/WeaponDetailPage.vue
@@ -3,26 +3,43 @@
     <LoadingSpinner v-if="loading" />
 
     <div v-else-if="weapon" class="weapon-detail">
-      <router-link to="/weapons">Back to weapons</router-link>
-      <h1>{{ weapon.name }}</h1>
-      <img :src="weapon.image_url" :alt="weapon.name" />
-      <p>{{ weapon.rarity }}</p>
-      <p>{{ weapon.weapon_type_id.name }}</p>
-      <p>{{ weapon.base_attack }}</p>
-      <p>{{ weapon.bonus_effect_type_id.name }}</p>
-      <p>
-        {{
-          formatBonusEffectValue(
-            weapon.bonus_effect_value,
-            weapon.bonus_effect_type_id.name
-          )
-        }}
-      </p>
-      <p>{{ weapon.release_date }}</p>
-      <p>{{ weapon.attribute }}</p>
-      <p>{{ weapon.attribute_description }}</p>
+      <div class="weapon-detail-header">
+        <div class="weapon-image-container">
+          <img
+            class="weapon-image"
+            :class="{
+              'rarity-5': weapon.rarity === 5,
+              'rarity-4': weapon.rarity === 4,
+            }"
+            :src="weapon.image_url"
+            :alt="weapon.name"
+          />
+        </div>
+        <div class="weapon-info">
+          <h1 class="weapon-name">{{ weapon.name }}</h1>
+          <p class="rarity-text text-white" :data-stars="weapon.rarity"></p>
+          <strong>{{ weapon.weapon_type_id.name }}</strong>
+          <p class="weapon-sub-stats">
+            <strong>Base Attack:</strong> {{ weapon.base_attack }}
+          </p>
+          <p class="weapon-sub-stats">
+            <strong>{{ weapon.bonus_effect_type_id.name }}:</strong>
+            {{
+              formatBonusEffectValue(
+                weapon.bonus_effect_value,
+                weapon.bonus_effect_type_id.name
+              )
+            }}
+          </p>
+        </div>
+      </div>
+      <h1 class="divider header">Attribute</h1>
+      <div class="weapon-attribute-text">
+        <h1>{{ weapon.attribute }}</h1>
+        <MarkdownRender class="pl-3 mt-2 mb-10" :content="weapon.attribute_description" />
+      </div>
       <div class="lore-text">
-        <h2>Lore</h2>
+        <h2 class="divider header">Lore</h2>
         <MarkdownRender :content="weapon.lore_description" />
       </div>
     </div>
@@ -81,10 +98,75 @@ onMounted(async () => {
 
 <style scoped>
 .weapon-detail-container {
-  margin: 75px 0 0 0;
+  margin: 100px 0 0 0;
 }
 
 .weapon-detail {
   background-color: var(--secondary);
+  width: 66%;
+  height: auto;
+  margin: 0 auto;
+  padding: 50px;
+  border-radius: 20px;
+}
+
+.weapon-detail-header {
+  display: flex;
+  justify-content: left;
+}
+
+.weapon-image {
+  width: 200px;
+  height: 200px;
+  margin-right: 20px;
+  border-radius: 20px;
+}
+
+.weapon-name {
+  font-family: var(--font-acme);
+}
+
+.weapon-info {
+  margin-left: 20px;
+  font-size: 1.5em;
+}
+
+.rarity-5 {
+  background: linear-gradient(145deg, #e7944a, #b56a2b);
+  box-shadow: 0px 0px 5px rgba(231, 148, 74, 0.8),
+    0px 0px 15px rgba(231, 148, 74, 0.5);
+}
+
+.rarity-4 {
+  background: linear-gradient(145deg, #9b72d5, #7149a3);
+  box-shadow: 0px 0px 5px rgba(155, 114, 213, 0.8),
+    0px 0px 15px rgba(155, 114, 213, 0.5);
+}
+
+.weapon-sub-stats {
+  font-family: var(--font-acme);
+  letter-spacing: 1px;
+}
+
+.header {
+  font-family: var(--font-acme);
+  font-size: 2.5em;
+  margin-top: 20px;
+  letter-spacing: 1px;
+}
+
+.weapon-attribute-text {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.lore-text {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+strong {
+  font-weight: bold;
+  color: lightgray;
+  letter-spacing: 1px;
+  font-size: 26px;
 }
 </style>
