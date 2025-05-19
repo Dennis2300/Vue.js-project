07df45fc175b138f0197c150f9a0b919d461afa6
Densanity <denniszixiang@gmail.com>
2025-05-19 20:07:19 +0200

    Update ArtifactsPage.vue

--- src/views/ArtifactsPage.vue
+++ src/views/ArtifactsPage.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="artifacts-container">
+  <div class="artifacts-page-container">
     <LoadingSpinner v-if="loading" />
-    <h1>Welcome to the Artifacts Page</h1>
+    <h1 class="artifact-page-header">Welcome to the Artifacts Page</h1>
     <div v-if="!loading && !error">
       <div v-for="artifact in artifacts" :key="artifact.id">
-        <div>
-          <img :src="artifact.flower_url" alt="">
-          <img :src="artifact.plume_url" alt="">
-          <img :src="artifact.sands_url" alt="">
-          <img :src="artifact.goblet_url" alt="">
-          <img :src="artifact.circlet_url" alt="">
-          <h2>{{ artifact.name }}</h2>
-          <p>{{ artifact.two_piece_set }}</p>
-          <p>{{ artifact.four_piece_set }}</p>
-          <p>{{ artifact.source }}</p>
+        <div class="artifacts-display">
+          <img class="artifact-img" :src="artifact.flower_url" :alt="artifact.name" />
+          <img class="artifact-img" :src="artifact.plume_url" :alt="artifact.name" />
+          <img class="artifact-img" :src="artifact.sands_url" :alt="artifact.name" />
+          <img class="artifact-img" :src="artifact.goblet_url" :alt="artifact.name" />
+          <img class="artifact-img" :src="artifact.circlet_url" :alt="artifact.name" />
         </div>
+        <h2>{{ artifact.name }}</h2>
+        <p>{{ artifact.two_piece_set }}</p>
+        <p>{{ artifact.four_piece_set }}</p>
+        <p>{{ artifact.source }}</p>
       </div>
     </div>
   </div>
@@ -51,16 +51,30 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.artifacts-container {
+.artifacts-page-container {
+  margin-top: 75px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
 }
 
-h1 {
+.artifact-page-header {
   font-size: 2rem;
   color: white;
+}
+
+.artifacts-display {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.artifact-img {
+  width: 100px;
+  height: 100px;
+  margin: 10px;
 }
 </style>
