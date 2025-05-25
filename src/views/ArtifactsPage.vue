a30cc4a0816a80f2cda091427b7ae03fe151254f
Densanity <denniszixiang@gmail.com>
2025-05-25 19:21:49 +0200

    reworked artifact page
    now its a simple grid layout

--- src/views/ArtifactsPage.vue
+++ src/views/ArtifactsPage.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="artifacts-page-container">
+    <h1 class="divider mb-10">Artifact Page</h1>
+    <!--Loading for when fetching is in progress-->
     <LoadingSpinner v-if="loading" />
-    <div v-if="!loading && !error">
+
+    <!--Display data when loading is done-->
+    <div v-if="!loading && !error" class="artifacts-display-grid">
       <div v-for="artifact in artifacts" :key="artifact.id">
- 
+        <div class="artifact-card">
+          <img class="artifact-img" :src="artifact.flower_url" alt="" />
+          <h2 class="truncate artifact-name">
+            {{ artifact.name }}
+          </h2>
+        </div>
       </div>
     </div>
   </div>
@@ -42,11 +51,34 @@ onMounted(() => {
 
 <style scoped>
 .artifacts-page-container {
-  margin-top: 75px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  margin-top: 25px;
+  width: 100vw;
+  padding: 50px;
 }
 
+.artifacts-display-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+}
+
+.artifact-card {
+  background-color: var(--secondary);
+  padding: 25px;
+  width: 350px;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  border-radius: 25px;
+}
+
+.artifact-name {
+  padding-left: 5px;
+}
+
+.artifact-img {
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+}
 </style>
