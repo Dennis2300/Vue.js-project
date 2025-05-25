1b6df7e106108dc6f220307e699699a8866b1b98
Densanity <denniszixiang@gmail.com>
2025-05-25 18:36:48 +0200

    reworking artifacts page

---
+++ src/views/ArtifactDetailPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="artifact-detail-page">
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+</script>
+
+<style scoped>
+.artifact-detail-page {
+    background-color: aqua;
+}
+
+</style>
--- src/views/ArtifactsPage.vue
+++ src/views/ArtifactsPage.vue
@@ -3,51 +3,7 @@
     <LoadingSpinner v-if="loading" />
     <div v-if="!loading && !error">
       <div v-for="artifact in artifacts" :key="artifact.id">
-        <div class="artifacts-display">
-          <h2 class="divider my-7 artifact-name">
-            {{ artifact.name }}
-          </h2>
-          <div class="artifacts-img-container mb-3">
-            <img
-              class="artifact-img"
-              :src="artifact.flower_url"
-              :alt="artifact.name"
-            />
-            <img
-              class="artifact-img"
-              :src="artifact.plume_url"
-              :alt="artifact.name"
-            />
-            <img
-              class="artifact-img"
-              :src="artifact.sands_url"
-              :alt="artifact.name"
-            />
-            <img
-              class="artifact-img"
-              :src="artifact.goblet_url"
-              :alt="artifact.name"
-            />
-            <img
-              class="artifact-img"
-              :src="artifact.circlet_url"
-              :alt="artifact.name"
-            />
-          </div>
-          <div class="artifacts-text-container">
-            <div class="two-piece-set">
-              <h3 class="piece-title">2 Piece Set Bonus</h3>
-              <p class="piece-text mt-2 mb-3">{{ artifact.two_piece_set }}</p>
-            </div>
-            <div class="four-piece-set">
-              <h3 class="piece-title">4 Piece Set Bonus</h3>
-              <MarkdownRender
-                class="piece-text"
-                :content="artifact.four_piece_set"
-              />
-            </div>
-          </div>
-        </div>
+ 
       </div>
     </div>
   </div>
@@ -92,61 +48,5 @@ onMounted(() => {
   align-items: center;
   justify-content: center;
 }
-
-.artifacts-display {
-  background-color: var(--secondary);
-  padding: 15px;
-  margin-bottom: 50px;
-  border-radius: 25px;
-  width: 50vw;
-}
-
-.artifact-name {
-  font-size: 2.7em;
-  font-family: var(--font-acme);
-  letter-spacing: 1px;
-  font-weight: lighter;
-}
-
-.artifacts-img-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.artifacts-text-container {
-  font-family: Arial, Helvetica, sans-serif;
-  letter-spacing: 1px;
-}
-
-.two-piece-set {
-  width: 85%;
-  margin: 0 auto;
-  text-align: left;
-  margin-bottom: 10px;
-}
-
-.four-piece-set {
-  width: 85%;
-  margin: 0 auto;
-  text-align: left;
-  margin-bottom: 10px;
-}
-
-.piece-title {
-  font-size: 1.5rem;
-  color: var(--tertiary);
-}
-
-.piece-text {
-  font-size: 1.2rem;
-}
-
-.artifact-img {
-  width: 100px;
-  height: 100px;
-  margin: 10px;
-}
-
 
 </style>
