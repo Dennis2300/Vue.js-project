2f88f3f4d763f629d34c689ce43d2cef1cb6ec87
Densanity <denniszixiang@gmail.com>
2025-06-07 17:56:47 +0200

    Update WeaponsPage.vue
    reworked weapons page to a grid format

--- src/views/WeaponsPage.vue
+++ src/views/WeaponsPage.vue
@@ -4,9 +4,38 @@
 
   <!-- Weapons Container -->
   <div v-if="!loading && !error" class="weapons-page-container">
-    <!-- Footer -->
-    <div class="mt-10">
-      <Footer />
+    <div class="weapons-page-content">
+      <h1 class="weapon-page-header divider text-5xl">Weapons Archive</h1>
+      <div class="weapon-grid-container mt-10">
+        <div
+          v-for="weapon in weapons"
+          :key="weapon.id"
+          class="weapon-grid-item"
+          :class="{
+            'rarity-5': weapon.rarity === 5,
+            'rarity-4': weapon.rarity === 4,
+          }"
+        >
+          <!-- Weapon image -->
+          <img
+            :src="weapon.image_url"
+            :alt="weapon.name"
+            class="weapon-image mt-5"
+          />
+          <!-- weapon name -->
+          <div class="divider"></div>
+          <h2 class="weapon-name">{{ weapon.name }}</h2>
+          <!-- Weapon Tags -->
+          <div class="weapon-tags-container mt-5">
+            <p class="weapon-type-tag">
+              {{ weapon.weapon_type_id.name }}
+            </p>
+            <p class="weapon-bonus-effect-tag">
+              {{ weapon.bonus_effect_type_id.name }}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -21,7 +50,6 @@ import { ref, onMounted } from "vue";
 import { supabase } from "./../supabaseClient.js"; // Import the Supabase client
 import LoadingSpinner from "./../components/LoadingSpinner.vue"; // Import the loading spinner component
 import ErrorComponent from "./../components/ErrorComponent.vue"; // Import the error component
-import Footer from "@/components/Footer.vue";
 
 // states for loading and error
 const loading = ref(true);
@@ -71,7 +99,9 @@ async function getAllWeapons() {
   try {
     let { data, error: fetchError } = await supabase
       .from("weapons")
-      .select("*, weapon_type_id(name), bonus_effect_type_id(name)");
+      .select(
+        "id, name, rarity, image_url, weapon_type_id(name), bonus_effect_type_id(name)"
+      );
     if (fetchError) throw fetchError;
     weapons.value = data;
     setCachedData(cacheKey, data);
@@ -81,20 +111,6 @@ async function getAllWeapons() {
   } finally {
     loading.value = false;
   }
-}
-
-function formatBonusEffectValue(value, type) {
-  // add more types here
-  return [
-    "CRIT DMG",
-    "CRIT Rate",
-    "DEF %",
-    "ATK %",
-    "HP %",
-    "Energy Recharge",
-  ].includes(type)
-    ? `${value}%`
-    : value;
 }
 
 function sortWeaponsByReleaseDate() {
@@ -110,6 +126,70 @@ onMounted(() => {
 
 <style scoped>
 .weapons-page-container {
-  background-color: aqua;
+  min-height: 100vh;
+}
+
+.weapon-page-header {
+  font-family: var(--font-archivo);
+}
+
+.weapons-page-content {
+  max-width: 1600px;
+  padding: 20px;
+}
+
+.weapon-grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 25px;
+}
+
+.weapon-grid-item {
+  background-color: var(--secondary);
+  border-radius: 15px;
+  padding: 20px;
+  text-align: center;
+  transition: transform 0.3s ease;
+  height: 375px;
+}
+
+.weapon-image {
+  width: 200px;
+  height: 200px;
+  border-radius: 25px;
+}
+
+.weapon-name {
+  font-family: var(--font-acme);
+  letter-spacing: 1px;
+}
+
+.weapon-tags-container {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+}
+
+.weapon-type-tag,
+.weapon-bonus-effect-tag {
+  background-color: var(--primary);
+  color: var(--tertiary);
+  padding: 10px 15px;
+  border-radius: 5px;
+  font-size: 0.9rem;
+}
+
+.rarity-5 .weapon-image {
+  background: linear-gradient(145deg, #e7944a, #b56a2b);
+  box-shadow: 0px 0px 15px rgba(231, 148, 74, 0.8),
+    0px 0px 30px rgba(231, 148, 74, 0.5);
+}
+
+.rarity-4 .weapon-image {
+  background: linear-gradient(145deg, #9b72d5, #7149a3);
+  box-shadow: 0px 0px 15px rgba(155, 114, 213, 0.8),
+    0px 0px 30px rgba(155, 114, 213, 0.5);
 }
 </style>
