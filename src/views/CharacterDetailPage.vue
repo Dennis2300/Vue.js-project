2ce283e7b5815f7013cf0ec7ade410858c6045a5
Densanity <denniszixiang@gmail.com>
2025-06-15 17:57:16 +0200

    reworking character detail page

--- src/views/CharacterDetailPage.vue
+++ src/views/CharacterDetailPage.vue
@@ -12,31 +12,54 @@
         'rarity-4': character.rarity === 4,
       }"
     >
-      <!-- Character Detail Header -->
-      <div class="character-detail-header-container">
-        <img
-          :src="character.image_url"
-          alt="Vision"
-          class="character-detail-avatar"
-        />
-        <p class="rarity-text text-white" :data-stars="character.rarity"></p>
-        <h1 class="character-detail-header">{{ character.name }}</h1>
-      </div>
-
-      <!-- Character Detail Layout -->
-      <div class="character-detail-layout">
-        <!-- Main Section -->
-        <CharacterDetailsMainSection :character="character" />
-
-        <!-- Side Sections -->
-        <CharacterDetailsRightSection :character="character" />
-
-        <!-- Bottom Section -->
-        <section class="bottom-section">
-          <div>
-            <a :href="character.wiki_url || '#'" target="_blank">Source</a>
+      <!-- character detail image container -->
+      <div class="character-detail-image-container">
+        <!-- character avatar -->
+        <div class="character-avatar-container">
+          <img
+            class="character-avatar"
+            :src="character.image_url"
+            :alt="character.name"
+          />
+          <h1 class="character-name mt-3">{{ character.name }}</h1>
+          <div class="character-tags-container mt-2">
+            <p class="character-tags">{{ character.vision.name }}</p>
+            <p class="character-tags">{{ character.team_role.name }}</p>
+            <p class="character-tags">{{ character.substat.name }}</p>
+            <p class="character-tags">{{ character.weapon_type.name }}</p>
           </div>
-        </section>
+        </div>
+        <!-- character overview -->
+        <div class="character-overview-container">
+          <div class="character-overview">
+            <!-- character voice actors -->
+            <h1 class="divider">Voice Actors</h1>
+            <div v-for="(name, lang) in character.va" :key="lang">
+              <p class="character-va">{{ lang }} - {{ name }}</p>
+            </div>
+
+            <!-- character affiliation -->
+            <h1 class="divider">Affiliation</h1>
+            <div
+              v-for="affiliation in character.affiliation"
+              :key="affiliation"
+            >
+              <p class="character-va">{{ affiliation }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- character detail container  -->
+      <div class="character-info-container">
+        <h1>{{ character.name }}</h1>
+        <p>{{ character.rarity }}</p>
+        <p>{{ character.birthday }}</p>
+        <p>{{ character.constellation }}</p>
+        <p>{{ character.affiliation }}</p>
+        <p>{{ character.favourite_dish }}</p>
+        <p>{{ character.release_date }}</p>
+        <p>{{ character.va }}</p>
+        <p>{{ character.wiki_url }}</p>
       </div>
     </div>
 
@@ -53,17 +76,9 @@ import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { supabase } from "./../supabaseClient.js";
 
-// Import the necessary styles
-import "./../css/CharacterDetailPage.css";
-import "./../css/CharacterDetailGrid.css";
-
 // Import the loading spinner component
 import "ldrs/trefoil";
 import LoadingSpinner from "./../components/LoadingSpinner.vue"; // Import the loading spinner component
-
-// Import the character details main section component
-import CharacterDetailsMainSection from "../components/CharacterDetailsMainSection.vue"; // Import the character details main section component
-import CharacterDetailsRightSection from "../components/CharacterDetailsRightSection.vue"; // Import the character details right section component
 
 const route = useRoute();
 const character = ref(null);
@@ -107,7 +122,9 @@ async function fetchCharacterDetails(characterId) {
     // First fetch character details
     const { data: characterData, error: charError } = await supabase
       .from("characters")
-      .select("*, vision:vision(name, image_url)")
+      .select(
+        "*, vision:vision(name, image_url), team_role:team_role(name), substat:substat(name), weapon_type:weapon_type(name)"
+      )
       .eq("id", characterId)
       .single();
     if (charError) throw charError;
@@ -158,3 +175,95 @@ onMounted(async () => {
   loading.value = false;
 });
 </script>
+
+<style scoped>
+.character-detail-container {
+  background-color: var(--secondary);
+  width: 1500px;
+  min-height: 100vh;
+}
+
+.character-detail-image-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.character-avatar-container {
+  background-color: blueviolet;
+  width: 700px;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.character-avatar {
+  width: 200px;
+  height: 200px;
+  border-radius: 15px;
+  padding: 10px 10px 0px 10px;
+}
+
+.character-name {
+  font-family: var(--font-fugaz);
+  font-size: 3rem;
+  margin-top: 10px;
+  text-transform: capitalize;
+}
+
+.character-tags-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+}
+
+.character-tags {
+  background-color: var(--primary);
+  color: var(--tertiary);
+  font-family: var(--font-acme);
+  padding: 10px 15px;
+  border-radius: 5px;
+  font-size: 0.9rem;
+  letter-spacing: 1px;
+}
+
+.rarity-5 .character-avatar {
+  background: linear-gradient(145deg, #e7944a, #b56a2b);
+  box-shadow: 0px 0px 15px rgba(231, 148, 74, 0.8),
+    0px 0px 30px rgba(231, 148, 74, 0.5);
+}
+
+.rarity-4 .character-avatar {
+  background: linear-gradient(145deg, #9b72d5, #7149a3);
+  box-shadow: 0px 0px 15px rgba(155, 114, 213, 0.8),
+    0px 0px 30px rgba(155, 114, 213, 0.5);
+}
+
+.character-overview-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: darkblue;
+  width: 700px;
+  height: 500px;
+}
+
+.character-overview {
+  background-color: #b56a2b;
+  padding: 10px 25px 10px 25px;
+  width: 500px;
+  border-radius: 15px;
+}
+
+.character-va {
+  font-family: var(--font-acme);
+  font-size: 1.2rem;
+  margin: 5px 0;
+}
+
+.character-info-container {
+  background-color: #b56a2b;
+}
+</style>
